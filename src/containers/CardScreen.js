c6c14955aa09d8678d57bf284dabd0e3158d3969
Frank <yuohuo@gmail.com>
2018-11-20 19:05:27 +1100

    only render cards on selected page

--- src/containers/CardScreen.js
+++ src/containers/CardScreen.js
@@ -1,3 +1,9 @@
 import { connect } from 'react-redux'
 import cardGrid from '../components/cardGrid'
- export default connect((state) => {return {cards: state.cards}})(cardGrid); 
+
+const mapStateToProps = (state) => {
+    const page = state.viewer.currentPage;
+    const cards = state.cards.slice(page * 12, (page + 1) * 12)
+    return {cards};
+}
+export default connect(mapStateToProps)(cardGrid); 
