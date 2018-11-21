03a1fb5620e59db26b58b615e992ae629c7b6d4f
Frank <yuohuo@gmail.com>
2018-11-21 13:39:51 +1100

    fix - App.js add card drawer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Paginator from './components/Paginator'
 import JssProvider from 'react-jss/lib/JssProvider';
 import { create } from 'jss';
 import { createGenerateClassName, jssPreset } from '@material-ui/core/styles';
-import CardScreen from './containers/CardScreen'
+import CardScreen from './containers/CardScreenContainer'
+import CardDrawer from './containers/CardDrawerContainer'
+import Paginator from './containers/PaginatorContainer';
 
 const generateClassName = createGenerateClassName();
 const jss = create(jssPreset());
+
 class App extends Component {
   render() {
     return (
-
       <JssProvider jss={jss} generateClassName={generateClassName}>
       <div className="App">
         <CardScreen />
         <Paginator />
+        <CardDrawer />
       </div>
     </JssProvider>
     );
